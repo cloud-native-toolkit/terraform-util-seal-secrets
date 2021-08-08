732a60d48a52d0ccd6934ed0f8aef7790910076b
Sean Sundberg <seansund@us.ibm.com>
2021-08-07 23:08:28 -0500

    Adds logic to generate secrets (#1)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ scripts/seal-secrets.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
+
+SOURCE_DIR="$1"
+DEST_DIR="$2"
+SEALED_SECRETS_FILE="$3"
+
+if [[ -z "${TMP_DIR}" ]]; then
+  TMP_DIR=".tmp"
+fi
+
+SEALED_SECRETS_DIR=$(dirname "${SEALED_SECRETS_FILE}")
+
+mkdir -p "${TMP_DIR}"
+mkdir -p "${DEST_DIR}"
+mkdir -p "./bin"
+mkdir -p "${SEALED_SECRETS_DIR}"
+
+if [[ -z "${KUBESEAL_CERT}" ]]; then
+  echo "KUBESEAL_CERT not provided as an environment variable"
+  exit 1
+fi
+
+KUBESEAL_CERT_FILE="${TMP_DIR}/kubeseal.cert"
+echo "${KUBESEAL_CERT}" > "${KUBESEAL_CERT_FILE}"
+
+KUBESEAL=$(command -v kubeseal | command -v ./bin/kubeseal)
+if [[ -z "${KUBESEAL}" ]]; then
+  BIN_DIR=$(cd ./bin; pwd -P)
+  mkdir -p "${BIN_DIR}" && curl -Lo "${BIN_DIR}/kubeseal" https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.16.0/kubeseal-linux-amd64
+  chmod +x "${BIN_DIR}/kubeseal"
+  KUBESEAL="${BIN_DIR}/kubeseal"
+fi
+
+echo "Kubeseal cert"
+cat "${KUBESEAL_CERT_FILE}"
+
+# Create an empty file for the sealed secrets
+echo -n "" > "${SEALED_SECRETS_FILE}"
+
+find "${SOURCE_DIR}" -name "*.yaml" | while read -r file; do
+  filename=$(basename "${file}")
+
+  ${KUBESEAL} --cert "${KUBESEAL_CERT_FILE}" --format yaml < "${file}" > "${DEST_DIR}/${filename}"
+  echo "- ${DEST_DIR}/${filename}" >> "${SEALED_SECRETS_FILE}"
+done
+
+echo "Generated sealed secrets"
+cat "${SEALED_SECRETS_FILE}"
--- variables.tf
+++ variables.tf
@@ -1,6 +1,20 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "source_dir" {
+  type        = string
+  description = "The directory containing the secrets that will be encrypted using the cert"
+}
 
+variable "dest_dir" {
+  type        = string
+  description = "The directory where the sealed secrets will be written. If the directory does not exist it will be created"
+}
+
+variable "kubeseal_cert" {
+  type        = string
+  description = "The certificate/public key used to encrypt the sealed secrets"
+  default     = ""
+}
+
+variable "label" {
+  type        = string
+  description = "The label for the collection of secrets"
+}

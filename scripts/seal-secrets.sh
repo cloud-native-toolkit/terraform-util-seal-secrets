89ea0ce5c2db745632082c794962a528e380f7b6
Sean Sundberg <seansund@us.ibm.com>
2022-03-03 17:17:02 -0600

    Removes file read breaking destroy and adds test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/seal-secrets.sh
+++ scripts/seal-secrets.sh
@@ -5,18 +5,13 @@ MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
 SOURCE_DIR="$1"
 DEST_DIR="$2"
-SEALED_SECRETS_FILE="$3"
 
 if [[ -z "${TMP_DIR}" ]]; then
   TMP_DIR=".tmp"
 fi
 
-SEALED_SECRETS_DIR=$(dirname "${SEALED_SECRETS_FILE}")
-
 mkdir -p "${TMP_DIR}"
 mkdir -p "${DEST_DIR}"
-mkdir -p "./bin"
-mkdir -p "${SEALED_SECRETS_DIR}"
 
 if [[ -z "${KUBESEAL_CERT}" ]]; then
   echo "KUBESEAL_CERT not provided as an environment variable"
@@ -36,15 +31,8 @@ fi
 echo "Kubeseal cert"
 cat "${KUBESEAL_CERT_FILE}"
 
-# Create an empty file for the sealed secrets
-echo -n "" > "${SEALED_SECRETS_FILE}"
-
 find "${SOURCE_DIR}" -name "*.yaml" | while read -r file; do
   filename=$(basename "${file}")
 
   ${KUBESEAL} --cert "${KUBESEAL_CERT_FILE}" --format yaml < "${file}" > "${DEST_DIR}/${filename}"
-  echo "- ${DEST_DIR}/${filename}" >> "${SEALED_SECRETS_FILE}"
 done
-
-echo "Generated sealed secrets"
-cat "${SEALED_SECRETS_FILE}"
--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,3 +1,8 @@
-//module "seal_secrets" {
-//  source = "./module"
-//}
+module "seal_secrets" {
+  source = "./module"
+
+  source_dir = "${path.cwd}/secrets"
+  dest_dir = "${path.cwd}/sealed-secrets"
+  label = "test"
+  kubeseal_cert = module.cert.cert
+}
